70cce24f1ed6c8cf47a92d98bd0e00c3eb4208ab
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-01-25 12:14:06 +0900

    TODO: RecycleView Listlayout Detaillayout

---
+++ app/src/main/java/com/example/pokedex/PokemonModel.java
@@ -0,0 +1,19 @@
+package com.example.pokedex;
+
+public class PokemonModel {
+    private int id;
+    private String en_name;
+    private String jp_name;
+    private String[] type;
+    private String HP;
+    private String attack;
+    private String defence;
+    private String sp_atk;
+    private String sp_def;
+    private String speed;
+
+    public PokemonModel(int id,String en_name,String jp_name,String[] type,String HP
+            ,String attack,String defence, String sp_atk, String sp_def,String speed ){
+        
+    }
+}
